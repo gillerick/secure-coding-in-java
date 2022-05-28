1b3f4d417525e1521689a82b84ca35e467f8e627
Gill Erick <erick@sendyit.com>
2022-05-28 19:46:49 +0300

    SCIJ: Build obfuscation util

---
+++ src/utils/ObfuscationUtil.java
@@ -0,0 +1,31 @@
+package utils;
+
+
+import org.apache.commons.codec.binary.Hex;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+
+class ObfuscationUtil {
+  private static final Logger LOGGER = LoggerFactory.getLogger(ObfuscationUtil.class);
+
+  private ObfuscationUtil() {
+    throw new IllegalStateException("Obfuscation class");
+  }
+
+  private static String obfuscateData(String data) {
+    try {
+      MessageDigest digest = MessageDigest.getInstance("SHA-256");
+      byte[] encodeHash = digest.digest(data.getBytes(StandardCharsets.UTF_8));
+      return new String(Hex.encodeHex(encodeHash));
+    } catch (Exception e) {
+      LOGGER.error("Error hashing data. Outputting empty string instead");
+      return "";
+    }
+
+  }
+}
+
+
